0712df500c60c0324ebec64586d3ad9de0b4e9c1
Karl J. Obermeyer <karl.obermeyer@gmail.com>
2016-04-16 18:12:22 -0600

    Add scripts for generating boilerplate python package, module, and scripts.

---
+++ scripts/python_templates/script.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+'''
+'''
+
+# Standard
+#import sys
+#import os
+#import re
+from pprint import pprint
+import time
+# start_time = time.time()
+# . . .
+# print 'Elapsed time =', time.time() - start_time, 's'
+
+# Scientific Computing and Visualization
+import numpy as np
+#import scipy.linalg
+#import scipy.stat
+#import scipy.special
+#import matplotlib
+#from matplotlib import pyplot as plt
+#plt.rcParams['figure.figsize'] = 10, 10 # default fig size
+#thismanager = plt.get_current_fig_manager()
+#thismanager.window.wm_geometry("+450+80") # works for backend 'TkAgg' only
+#from matplotlib import animation
+
+# Poject-Specific
+# import $A
+
+
+def main():
+
+    np.random.seed(13)
+    start_time = time.time()
+
+    # . . .
+
+    print 'Elapsed time =', time.time() - start_time, 's'
+
+
+if __name__ == "__main__":
+    main()
