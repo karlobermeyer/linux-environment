98715becad64aca202f2c1a6bbb21b5a22d4a053
Karl J. Obermeyer <karl.obermeyer@gmail.com>
2017-04-14 21:43:29 -0600

    Add python 3 scripts and templates.

---
+++ scripts/python3_templates/module.py
@@ -0,0 +1,26 @@
+'''
+'''
+
+# Standard
+#import sys
+#import os
+#import re
+from pprint import pprint
+#import time
+# start_time = time.time()
+# . . .
+# print('Elapsed time =', time.time() - start_time, 's')
+
+# Scientific Computing and Visualization
+import numpy as np
+#import scipy.linalg
+#import scipy.stat
+#import scipy.special
+#import matplotlib
+#from matplotlib import pyplot as plt
+#from matplotlib import animation
+
+# Poject-Specific
+# . . .
+
+pass
