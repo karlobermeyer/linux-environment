9f88ad92d004d446078072acf67e58585b05507c
Karl J. Obermeyer <karl.obermeyer@gmail.com>
2018-02-13 18:44:09 -0700

    Add back_up_identity.sh.

---
+++ back_up_identity.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# To unpack `identity_backup.tgz`.
+# $ tar -xvzf identity_backup.tgz
+
+# To shred storage device used to transport `identity_backup.tgz`.
+# $ sudo fdisk -l  # Find device, e.g., /dev/sdb.
+# $ sudo shred -vz -n2 /dev/sdb  # Shred.
+
+# Copy files to sync'd folder.
+cp -R ~/.ssh ~/Sync/
+cp -R ~/.gnupg ~/Sync/ 2>/dev/null  # Suppress "Operation not supported on socket" errors.
+cp -R ~/.bashrc_private ~/Sync/
+cp -R ~/README_IF_FOUND.txt ~/Sync/
+
+# Create gzip'd tar ball for transport to another system.
+rm -rf identity_backup
+mkdir identity_backup
+cp -R ~/.ssh identity_backup/
+cp -R ~/.gnupg identity_backup/
+cp -R ~/.bashrc_private identity_backup/
+cp -R ~/README_IF_FOUND.txt identity_backup/
+tar -cvzf identity_backup.tgz identity_backup
+rm -rf identity_backup
