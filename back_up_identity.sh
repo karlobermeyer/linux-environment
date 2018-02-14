6cdf6abc0306f6b692d958e6f642d5feb9ae8533
Karl J. Obermeyer <karl.obermeyer@gmail.com>
2018-02-13 19:33:10 -0700

    Clean up back_up_identity.sh.

--- back_up_identity.sh
+++ back_up_identity.sh
@@ -7,14 +7,25 @@
 # $ sudo fdisk -l  # Find device, e.g., /dev/sdb.
 # $ sudo shred -vz -n2 /dev/sdb  # Shred.
 
+clear
+
 # Copy files to sync'd folder.
-cp -R ~/.ssh ~/Sync/
-cp -R ~/.gitconfig ~/Sync/
-cp -R ~/.gnupg ~/Sync/ 2>/dev/null  # Suppress "Operation not supported on socket" errors.
-cp -R ~/.bashrc_private ~/Sync/
-cp -R ~/README_IF_FOUND.txt ~/Sync/
+echo ""
+if [ -d "${HOME}/Sync" ]
+then
+    echo "Copying files to Sync directory."
+    cp -R ~/.ssh ~/Sync/
+    cp -R ~/.gitconfig ~/Sync/
+    cp -R ~/.gnupg ~/Sync/ 2>/dev/null  # Suppress "Operation not supported on socket" errors.
+    cp -R ~/.bashrc_private ~/Sync/
+    cp -R ~/README_IF_FOUND.txt ~/Sync/
+else
+    echo "Sync directory not found."
+fi
 
 # Create gzip'd tar ball for transport to another system.
+echo ""
+echo "Creating gzip'd tar ball for transport to another system."
 rm -rf identity_backup
 mkdir identity_backup
 cp -R ~/.ssh identity_backup/
@@ -24,3 +35,4 @@ cp -R ~/.bashrc_private identity_backup/
 cp -R ~/README_IF_FOUND.txt identity_backup/
 tar -cvzf identity_backup.tgz identity_backup
 rm -rf identity_backup
+echo ""
